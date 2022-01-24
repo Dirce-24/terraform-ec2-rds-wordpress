380c63d9092dcfb9cc54fa0174c2487c67a100a2
Dirce-24 <78606977+Dirce-24@users.noreply.github.com>
2022-01-24 20:09:12 +0200

    Create provider.tf
    create files

---
+++ provider.tf
@@ -0,0 +1,16 @@
+// Provider configuration
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
